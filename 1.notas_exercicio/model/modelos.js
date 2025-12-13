4744970e68885480a160f9a78fb6ba04ea5be0b3
Michael-Rafael-Andrade <michaelrafaeldeandrade@gmail.com>
2025-12-13 00:13:53 -0300

    modelo.js update - criando os atributos dentro do BD
    modelo.js update - criando os atributos dentro do BD

--- 1.notas_exercicio/model/modelos.js
+++ 1.notas_exercicio/model/modelos.js
@@ -1,34 +1,31 @@
-// Neste arquvio serão definidos todos os modelos de dados da aplicação 
-class Nota {
-    constructor(chave, titulo, texto, status){
-        this._chave = chave;
-        this._titulo = titulo;
-        this._texto = texto;
-        this._status = status;
-    }
-    get status(){
-        return this._status;
-    }
+// model/modelos.js
+const { DataTypes, Model } = require('sequelize');
+const sequelize = require('./server.js');
 
-    set status(novoStatus){
-        this._status = novoStatus;
-    }
+class Nota extends Model { } // classe herdando de 'Model'
 
-    get chave(){
-        return this._chave;
-    }
-    get titulo(){
-        return this._titulo;
-    }
-    get texto(){
-        return this._texto;
-    }
-    set titulo(novoTitulo){
-        this._titulo = novoTitulo;
-    }
-    set texto(novoTexto){
-        this._texto = novoTexto;
-    }
-}
+Nota.init( // construtor com a definição dos atributos
+    {
+        // substituindo o atributo 'chave'
+        id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, allowNull: false },
+        titulo: { type: DataTypes.STRING, unique: true, allowNull: false },
+        texto: { type: DataTypes.TEXT, allowNull: false },
+        importancia: { type: DataTypes.INTEGER, allowNull: false },
+        lida: { type: DataTypes.BOOLEAN, defaultValue: false },
+    },
+    {   // configurações adicionais do modelo
+        sequelize, // para estabelecer conexão com BD
+        freezeTableName: true, // nome da tabela igual ao nome da classe
+        createdAt: 'criada_em', // nome do atributo 'createdAt'
+        updatedAt: 'atualizada_em', // nome do atributo 'updatedAt'
+    },
+);
 
-module.exports = Nota
+// cria um modelo par sincronizar com o banco de dados
+sequelize.sync({ alter: true }).then(() => { // alter: true, para aplicar alterações de código no BD
+    console.log('Modelos sincronizados com o banco de dados.');
+}).catch((error) => {
+    console.error('Erro ao sincronizar modelos com o banco de dados: ', error);
+});
+
+module.exports = Nota;
